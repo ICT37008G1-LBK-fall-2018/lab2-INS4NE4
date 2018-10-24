17b33dc156b8797078835ef08e27e28de5145aaf
unknown <email@example.com>
2018-10-24 06:22:44 +0400

    L204

--- L204/javas.js
+++ L204/javas.js
@@ -1,10 +1,9 @@
 var vaule = 24;
 
-while (true){
-x=prompt("Please Enter Password:", '');
-if (x==vaule)
-{
-    alert ('true');
-    break;
-}
+while (true) {
+    x = prompt("Please Enter Password:", '');
+    if (x == vaule) {
+        alert('true');
+        break;
+    }
 }
